f7d090108e25abc936a1315e48a3916515f0be4b
Andrew Fryzel <50430261+andrewfryzel@users.noreply.github.com>
2019-05-09 10:45:50 -0600

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = 60;
+            Timer sw = new Timer();
+            sw.Interval = 1000;
+            sw.Elapsed += tick;
+            sw.Start();
+
+            while (time != 0) ;
+
+            sw.Stop();
+            Console.ReadKey();
+
+            void tick(object sender, ElapsedEventArgs e)
+            {
+                Console.WriteLine("Time: " + time);
+                time--;
+            }
+        }
+
+    }
+}
